5a7820f074c036edbedf16b0806cfea92415a62f
Matt Keller <mattkeller@Matts-MBP.hsd1.mn.comcast.net>
2025-02-12 11:17:50 -0600

    Add google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import { Space_Grotesk } from 'next/font/google'
 import './globals.css'
 import { SpeedInsights } from '@vercel/speed-insights/next'
 import { Analytics } from '@vercel/analytics/react'
+import Script from 'next/script'
 
 const spaceGrotesk = Space_Grotesk({
   variable: '--font-space-grotesk',
@@ -16,7 +17,25 @@ export default function RootLayout({
   return (
     <html lang="en">
       <SpeedInsights />
+      {/* Vercel analytics */}
       <Analytics />
+
+      {/* Google Analytics */}
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=G-CE5KD70FXZ`}
+        strategy="afterInteractive"
+      />
+      <Script
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-CE5KD70FXZ');`,
+        }}
+        id="gtag-init"
+        strategy="lazyOnload"
+      />
       <body className={`${spaceGrotesk.variable} antialiased`}>{children}</body>
     </html>
   )
