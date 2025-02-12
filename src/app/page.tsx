8735cbb09c61864ebdee4af9118f4ad8964522e0
Matt Keller <mattkeller@Matts-MBP.hsd1.mn.comcast.net>
2025-02-12 17:52:55 -0600

    Layout fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,15 +7,15 @@ import { ModeToggle } from '@/components/custom/ModeToggle'
 export default function Home() {
   return (
     <>
-      <div className="fixed right-4 top-4">
+      <div className="fixed right-4 top-4 hidden sm:block">
         <ModeToggle />
       </div>
       <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 sm:p-20">
-        <header>
+        <header className="text-center">
           <h1 className={cn('text-4xl font-bold')}>Am I Drinking Enough Water?</h1>
         </header>
-        <main className="row-start-2 flex flex-col items-center gap-4 sm:items-start">
-          <div className="flex w-full flex-col gap-4 lg:flex-row">
+        <main className="row-start-2 flex flex-col items-center gap-4">
+          <div className="flex w-full flex-col items-center gap-4 lg:flex-row lg:items-start">
             <div className="w-full lg:w-2/3">
               <InputsCard />
             </div>
